9c0b254e21335ba03dfdf3c939bed711ec1a9bfe
Alexander Zhmurkov <a.zhmurkov@mail.ru>
2024-10-07 21:58:03 +0300

    solved task1

--- task1.js
+++ task1.js
@@ -1,5 +1,11 @@
 var twoSum = function (nums, target) {
-    const =
+    for (let i = 0; i <= nums.length; i++) {
+        for (let j = i + 1; j <= nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
